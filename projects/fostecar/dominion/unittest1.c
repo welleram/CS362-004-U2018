968be7c9277d73ace6ba9c05df6276f3c7274c36
Carissa Foster <fostecar@flip2.engr.oregonstate.edu>
2018-07-15 18:58:29 -0700

    added unittest1 (getCost()) and unittest2 (fullDeckCount())

---
+++ projects/fostecar/dominion/unittest1.c
@@ -0,0 +1,152 @@
+/* -----------------------------------------------------------------------
+* Carissa Foster - CS362 - Assignment-3
+* This is a unit test implemented for the getCost() function.
+* Updated makefile to include:
+*
+* testGetCost: unittest1.c dominion.o rngs.o
+* gcc -o testGetCost -g testGetCost.c dominion.o rngs.o $(CFLAGS)
+* -----------------------------------------------------------------------
+*/
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "rngs.h"
+#include <time.h>
+
+
+
+int main() {
+
+	int cost = -5;
+	int i = -5;
+	int outOfBounds = -5;
+	int max = 1000;
+
+	printf("Begin Test - dominion.c, getCost() - unittest1.c\n");
+
+
+	/*Cycle through card numbers and compare to known cost.*/
+	for (i = 0; i < 27; ++i) {
+		cost = getCost(i);
+
+		if ((i == curse) && (cost == 0)) {
+			printf("PASS : card %d, cost %d is correct\n", curse, cost);
+		}
+		else if ((i == estate) && (cost == 2)) {
+			printf("PASS : card %d, cost %d is correct\n", estate, cost);
+		}
+		else if ((i == duchy) && (cost == 5)) {
+			printf("PASS : card %d, cost %d is correct\n", duchy, cost);
+		}
+		else if ((i == province) && (cost == 8)) {
+			printf("PASS : card %d, cost %d is correct\n", province, cost);
+		}
+		else if ((i == copper) && (cost == 0)) {
+			printf("PASS : card %d, cost %d is correct\n", copper, cost);
+		}
+		else if ((i == silver) && (cost == 3)) {
+			printf("PASS : card %d, cost %d is correct\n", silver, cost);
+		}
+		else if ((i == gold) && (cost == 6)) {
+			printf("PASS : card %d, cost %d is correct\n", gold, cost);
+		}
+		else if ((i == adventurer) && (cost == 6)) {
+			printf("PASS : card %d, cost %d is correct\n", adventurer, cost);
+		}
+		else if ((i == council_room) && (cost == 5)) {
+			printf("PASS : card %d, cost %d is correct\n", council_room, cost);
+		}
+		else if ((i == feast) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", feast, cost);
+		}
+		else if ((i == gardens) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", gardens, cost);
+		}
+		else if ((i == mine) && (cost == 5)) {
+			printf("PASS : card %d, cost %d is correct\n", mine, cost);
+		}
+		else if ((i == remodel) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", remodel, cost);
+		}
+		else if ((i == smithy) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", smithy, cost);
+		}
+		else if ((i == village) && (cost == 3)) {
+			printf("PASS : card %d, cost %d is correct\n", village, cost);
+		}
+		else if ((i == baron) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", baron, cost);
+		}
+		else if ((i == great_hall) && (cost == 3)) {
+			printf("PASS : card %d, cost %d is correct\n", great_hall, cost);
+		}
+		else if ((i == minion) && (cost == 5)) {
+			printf("PASS : card %d, cost %d is correct\n", minion, cost);
+		}
+		else if ((i == steward) && (cost == 3)) {
+			printf("PASS : card %d, cost %d is correct\n", steward, cost);
+		}
+		else if ((i == tribute) && (cost == 5)) {
+			printf("PASS : card %d, cost %d is correct\n", tribute, cost);
+		}
+		else if ((i == ambassador) && (cost == 3)) {
+			printf("PASS : card %d, cost %d is correct\n", ambassador, cost);
+		}
+		else if ((i == cutpurse) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", cutpurse, cost);
+		}
+		else if ((i == embargo) && (cost == 2)) {
+			printf("PASS : card %d, cost %d is correct\n", embargo, cost);
+		}
+		else if ((i == outpost) && (cost == 5)) {
+			printf("PASS : card %d, cost %d is correct\n", outpost, cost);
+		}
+		else if ((i == salvager) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", salvager, cost);
+		}
+		else if ((i == sea_hag) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", sea_hag, cost);
+		}
+		else if ((i == treasure_map) && (cost == 4)) {
+			printf("PASS : card %d, cost %d is correct\n", treasure_map, cost);
+		}
+		else {
+			printf("FAIL : card %d, cost %d is NOT correct\n", i, cost);
+		}
+	}
+
+	/*Test out of bounds card values. Return value expected to be -1.*/
+	srand(time(0));
+
+	for (i = 0; i < 4; ++i) {
+		if (i == 0) { //one beyond upper limit
+			outOfBounds = 27;
+		}
+		else if (i == 1) { //one beyond lower limit
+			outOfBounds = -1;
+		}
+		else if (i == 2) { //random beyond upper limit
+			outOfBounds = rand() % ((max + 1) - 28) + 28;
+		}
+		else if (i == 3) { // random beyond lower limit
+			outOfBounds = rand() % ((max + 1) - 2) + 2;
+			outOfBounds = outOfBounds * -1;
+		}
+		
+		cost = getCost(outOfBounds);
+
+		if (cost == -1) {
+			printf("PASS : input %d, return %d is correct\n", outOfBounds, cost);
+		}
+		else {
+			printf("FAIL : input %d, return %d is NOT correct\n", outOfBounds, cost);
+		}
+	}
+
+	printf("End Test - dominion.c, getCost() - unittest1.c\n");
+
+	return 1;
+}
---
+++ projects/fostecar/dominion/unittest2.c
@@ -0,0 +1,236 @@
+/* -----------------------------------------------------------------------
+* Carissa Foster - CS362 - Assignment-3
+* This is a unit test implemented for the fullDeckCount() function.
+* Updated makefile to include:
+*
+* testFullDeckCount: unittest2.c dominion.o rngs.o
+* gcc -o testGetCost -g unittest2.c dominion.o rngs.o $(CFLAGS)
+* -----------------------------------------------------------------------
+*/
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "rngs.h"
+#include <time.h>
+
+
+int main() {
+
+	struct gameState testGame;
+	testGame.numPlayers = MAX_PLAYERS;
+	int player = -5;
+	int card = -5;
+	int cardPos = -5;
+	int cardCount = -5;
+	int cardCountArray[27];
+	int i = -5;
+
+	srand(time(0));
+
+	/*Initialize cardCountArray*/
+	card = 0;
+	
+	for (i = 0; i < 27; ++i) {
+		cardCountArray[i] = 0;
+	}
+
+	for (i = 0; i < MAX_DECK; ++i) {
+		testGame.deck[0][i] = card;
+		testGame.hand[0][i] = card;
+		testGame.discard[0][i] = card;
+		testGame.deck[1][i] = card;
+		testGame.hand[1][i] = card;
+		testGame.discard[1][i] = card;
+		testGame.deck[2][i] = card;
+		testGame.hand[2][i] = card;
+		testGame.discard[2][i] = card;
+		testGame.deck[3][i] = card;
+		testGame.hand[3][i] = card;
+		testGame.discard[3][i] = card;
+
+		cardCountArray[card] = cardCountArray[card] + 1;
+
+		++card;
+		if (card > 26) {
+			card = 0;
+		}
+	}
+
+
+	printf("Begin Test - dominion.c, fullDeckCount() - unittest2.c\n");
+
+	/*Check various cards in the deck only*/
+	card = rand() % 27;  //Select a card (0,26)
+	player = 0;
+	testGame.deckCount[player] = MAX_DECK;
+	testGame.handCount[player] = 0;
+	testGame.discardCount[player] = 0;
+
+	cardCount = fullDeckCount(player, card, &testGame);
+
+	if (cardCount == cardCountArray[card]) {
+		printf("PASS : Deck Only, card %d, quantity %d, function return %d\n", card, cardCountArray[card], cardCount);
+	}
+	else {
+		printf("FAIL : Deck Only, card %d, quantity %d, function return %d\n", card, cardCountArray[card], cardCount);
+	}
+
+	/*Check card in the hand only*/
+	card = rand() % 27;  //Select a card (0,26)
+	player = 1;
+	testGame.deckCount[player] = 0;
+	testGame.handCount[player] = MAX_HAND;
+	testGame.discardCount[player] = 0;
+
+	cardCount = fullDeckCount(player, card, &testGame);
+
+	if (cardCount == cardCountArray[card]) {
+		printf("PASS : Hand Only, card %d, quantity %d, function return %d\n", card, cardCountArray[card], cardCount);
+	}
+	else {
+		printf("FAIL : Hand Only, card %d, quantity %d, function return %d\n", card, cardCountArray[card], cardCount);
+	}
+
+
+	/*Check card in the discard only*/
+	card = rand() % 27;  //Select a card (0,26)
+	player = 2;
+	testGame.deckCount[player] = 0;
+	testGame.handCount[player] = 0;
+	testGame.discardCount[player] = MAX_DECK;
+
+	cardCount = fullDeckCount(player, card, &testGame);
+
+	if (cardCount == cardCountArray[card]) {
+		printf("PASS : Discard Only, card %d, quantity %d, function return %d\n", card, cardCountArray[card], cardCount);
+	}
+	else {
+		printf("FAIL : Discard Only, card %d, quantity %d, function return %d\n", card, cardCountArray[card], cardCount);
+	}
+
+	/*Check card in the deck, hand, and discard*/
+	card = rand() % 27;  //Select a card (0,26)
+	player = 3;
+	testGame.deckCount[player] = MAX_DECK;
+	testGame.handCount[player] = MAX_HAND;
+	testGame.discardCount[player] = MAX_DECK;
+
+	cardCount = fullDeckCount(player, card, &testGame);
+
+	if (cardCount == (3 * cardCountArray[card])) {
+		printf("PASS : Deck/Hand/Discard, card %d, quantity %d, function return %d\n", card, (3 * cardCountArray[card]), cardCount);
+	}
+	else {
+		printf("FAIL : Deck/Hand/Discard, card %d, quantity %d, function return %d\n", card, (3 * cardCountArray[card]), cardCount);
+	}
+
+	/*Card NOT in deck, hand, and/or discard*/
+	player = 0;
+	card = rand() % 27;  //Select a card (0,26)
+
+	for (i = card; i < 26; ++i) {
+		testGame.deck[player][i] = testGame.deck[player][i + 1];
+		testGame.hand[player][i] = testGame.deck[player][i + 1];
+		testGame.discard[player][i] = testGame.deck[player][i + 1];
+	}
+
+	testGame.deckCount[player] = 26;
+	testGame.handCount[player] = 26;
+	testGame.discardCount[player] = 26;
+
+
+	cardCount = fullDeckCount(player, card, &testGame);
+
+	if (cardCount == 0) {
+		printf("PASS : Card NOT in deck/hand/discard, function return %d\n", cardCount);
+	}
+	else {
+		printf("FAIL : Card NOT in deck/hand/discard, function return %d\n", cardCount);
+	}
+
+	/*All cards*/
+	player = 1;
+	card = rand() % 27;  //Select a card (0,26)
+	testGame.deckCount[player] = MAX_DECK;
+	testGame.handCount[player] = MAX_HAND;
+	testGame.discardCount[player] = MAX_DECK;
+		
+	for (cardPos = 0; cardPos < MAX_DECK; ++cardPos) {
+		testGame.deck[player][cardPos] = card;
+		testGame.hand[player][cardPos] = card;
+		testGame.discard[player][cardPos] = card;
+	}
+
+	cardCount = fullDeckCount(player, card, &testGame);
+	if (cardCount == ((2 * MAX_DECK) + MAX_HAND)) {
+		printf("PASS : All Cards, player %d, card %d, function return %d\n", player, card, cardCount);
+	}
+	else {
+		printf("FAIL : All Cards, player %d, card %d, function return %d\n", player, card, cardCount);
+	}
+	
+
+	/*Player had zero cards*/
+	player = 2;
+	testGame.deckCount[player] = 0;
+	testGame.handCount[player] = 0;
+	testGame.discardCount[player] = 0;
+
+	cardCount = fullDeckCount(player, card, &testGame);
+	if (cardCount == 0) {
+		printf("PASS : Player has zero cards, player %d, card %d, function return %d\n", player, card, cardCount);
+	}
+	else {
+		printf("FAIL : Player has zero cards, player %d, card %d, function return %d\n", player, card, cardCount);
+	}
+	
+
+	/*Invalid player*/
+	card = rand() % 27;  //Select a card (0,26)
+	player = 4;
+	cardCount = fullDeckCount(player, card, &testGame);
+	printf("Behavior Check : Invalid Player (+1), return %d\n", cardCount);
+
+	player = -1;
+	cardCount = fullDeckCount(player, card, &testGame);
+	printf("Behavior Check : Invalid Player (-1), return %d\n", cardCount);
+
+	/*Invalid card*/
+	player = 2;
+	card = 27;
+	cardCount = fullDeckCount(player, card, &testGame);
+	printf("Behavior Check : Invalid Card (+1), return %d\n", cardCount);
+
+	card = -1;
+	cardCount = fullDeckCount(player, card, &testGame);
+	printf("Behavior Check : Invalid Card (-1), return %d\n", cardCount);
+
+
+	printf("End Test - dominion.c, fullDeckCount() - unittest2.c\n");
+
+	return 1;
+}
+/*
+struct gameState {
+	int numPlayers; //number of players
+	int supplyCount[treasure_map + 1];  //this is the amount of a specific type of card given a specific number.
+	int embargoTokens[treasure_map + 1];
+	int outpostPlayed;
+	int outpostTurn;
+	int whoseTurn;
+	int phase;
+	int numActions; // Starts at 1 each turn 
+	int coins;  // Use as you see fit! 
+	int numBuys; //  Starts at 1 each turn 
+	int hand[MAX_PLAYERS][MAX_HAND];
+	int handCount[MAX_PLAYERS];
+	int deck[MAX_PLAYERS][MAX_DECK];
+	int deckCount[MAX_PLAYERS];
+	int discard[MAX_PLAYERS][MAX_DECK];
+	int discardCount[MAX_PLAYERS];
+	int playedCards[MAX_DECK];
+	int playedCardCount;
+};*/
